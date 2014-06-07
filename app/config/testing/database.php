b4b496d06237f13f2178010bf43bd66c05fcc439
Omer Karadagli <ufokaradagli@gmail.com>
2014-06-07 13:55:58 +0100

    Add testing database connection

---
+++ app/config/testing/database.php
@@ -0,0 +1,17 @@
+<?php
+
+return array(
+	'default' => 'mysql',
+	'connections' => array(
+		'mysql' => array(
+			'driver'    => 'mysql',
+			'host'      => 'localhost',
+			'database'  => 'uygunphoto_test',
+			'username'  => 'root',
+			'password'  => '1234',
+			'charset'   => 'utf8',
+			'collation' => 'utf8_unicode_ci',
+			'prefix'    => '',
+		),
+	)
+);
